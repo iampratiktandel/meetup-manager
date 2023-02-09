f8dee3cadb8c02d911241e76adc522d1d9247128
PratikTandel <pratik.tandel@1rivet.com>
2023-02-09 12:08:33 +0530

    feat: add MeetupDetail component & update MeetupItem

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
-function MeetupDetail() {
-    return <h1>Hello</h1>
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1200px-Stadtbild_M%C3%BCnchen.jpg?20130611211153'
+            title='Pilot'
+            address='Munich, Germany'
+            description='This is a first meetup'
+        />
+    );
 }
 
-export default MeetupDetail;
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,32 @@ const DUMMY_MEETUPS = [
     }
 ];
 
-function HomePage() {
+function HomePage(props) {
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
     );
 }
+
+export async function getServerSideProps(context) {
+    const req = context.req;
+    const res = context.res;
+
+    // fetch data from an API
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    }
+}
+
+// export async function getStaticProps() {
+//     // fetch data from an API
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         },
+//         revalidate: 1
+//     };
+// }
 
 export default HomePage;
